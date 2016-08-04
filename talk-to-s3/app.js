e348951d895f50d48d6951b02c5bd303152e0d9b
Victor Plama <vpalma@tribpub.com>
2016-08-04 11:06:29 -0700

    Updates

---
+++ talk-to-s3/app.js
@@ -0,0 +1,27 @@
+
+var AWS = require('aws-sdk');
+// ec2-node-user
+// Access Key ID:
+// AKIAITBO3FCFAOIU77FA
+// Secret Access Key:
+// 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
+AWS.config.loadFromPath('./config.json');
+
+
+var s3 = new AWS.S3();
+
+ s3.createBucket({Bucket: 'www.commitdeploy.hash123321'}, function() {
+
+  var params = {Bucket: 'www.commitdeploy.hash123321', Key: 'AKIAITBO3FCFAOIU77FA', Body: 'Hello!'};
+
+  s3.putObject(params, function(err, data) {
+
+      if (err)
+
+          console.log(err)
+
+      else       console.log("Successfully uploaded data to myBucket/myKey");
+
+   });
+
+});
